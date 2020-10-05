c1d776a799d5ca7229f339ba9b13bd565a3c305a
poornakani <38127363+poornakani@users.noreply.github.com>
2020-10-05 23:13:34 +0530

    Add files via upload

---
+++ OAuthProject/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+    var clientId ="533397777612-penk33fllosubrlg2v88rt6crfee15rp.apps.googleusercontent.com" //client id which was created
+
+    var redirect_uri="http://localhost/OAuthProject/upload.html"
+
+    var scope="https://www.googleapis.com/auth/drive"
+
+    var url=""
+
+
+    $("#login").click(function(){
+
+        signIn(clientId,redirect_uri,scope,url);
+    
+    
+    })
+
+
+    function signIn(clientId,redirect_uri,scope,url){
+
+        url = "https://accounts.google.com/o/oauth2/v2/auth?redirect_uri="+redirect_uri
+        +"&prompt=consent&response_type=code&client_id="+clientId+"&scope="+scope
+        +"&access_type=offline";
+
+        window.location = url;
+        
+    }
+
+
+});
+
